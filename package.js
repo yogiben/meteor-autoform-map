e0c40129fc5b03ffb1aff4e84ac9e7958acd1435
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2015-04-01 15:50:58 +0000

    bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'yogiben:autoform-map',
   summary: 'Edit location coordinates with autoForm',
-  version: '0.0.3',
+  version: '0.0.4',
   git: 'https://github.com/yogiben/meteor-autoform-map'
 });
 

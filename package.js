7a91a56af548aceae60dd7e27405b8f8b942c809
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2015-06-11 10:25:42 +0000

    update to fix zoom

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'yogiben:autoform-map',
   summary: 'Edit location coordinates with autoForm',
-  version: '0.1.2',
+  version: '0.1.3',
   git: 'https://github.com/yogiben/meteor-autoform-map'
 });
 

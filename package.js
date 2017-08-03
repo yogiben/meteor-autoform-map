5e0408d79ea882d50441ae9db1dfc871bb5f8184
Roshdy <roshdy89@gmail.com>
2017-08-03 10:30:13 +0300

    Update
    - Increased package version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'roshdy:autoform-map',
   summary: 'Edit location coordinates with autoForm',
-  version: '2.0.0',
+  version: '2.0.1',
   git: 'https://github.com/Roshdy/meteor-autoform-map'
 });
 

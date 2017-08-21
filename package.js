e1daf8550a6ad330139e5b1ed98785e8adf370c6
Roshdy <roshdy89@gmail.com>
2017-08-21 20:16:24 +0300

    Fix
    - Fixed map-reset undefined when getting default location & current
    location
    - Updated Package version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'roshdy:autoform-map',
   summary: 'Edit location coordinates with autoForm',
-  version: '2.1.0',
+  version: '2.1.1',
   git: 'https://github.com/Roshdy/meteor-autoform-map'
 });
 
